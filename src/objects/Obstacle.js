5cbb02ec1210a8e0c8bbe2e2d45d894834a95e5d
Karina Alarcón <alarconkarina.dev@gmail.com>
2026-07-25 05:04:40 -0300

    modify position of obstacles

--- src/objects/Obstacle.js
+++ src/objects/Obstacle.js
@@ -3,16 +3,25 @@
 class Obstacle extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y, type) {
     super(scene, x, y, type);
+    
     scene.add.existing(this);
     scene.physics.add.existing(this);
 
     // Velocidad negativa para движ izquierda
-    this.setVelocityX(-200);
+    this.setVelocityX(-300);
     this.setImmovable(true);
     this.body.allowGravity = false;
+
+    console.log(
+  "allowGravity:",
+  this.body.allowGravity,
+  "gravityY:",
+  this.body.gravity.y
+);
   }
 
   update() {
+
     // Destruir cuando salga por el borde izquierdo
     if (this.x < -this.width) {
       this.destroy();
